6753d79ccc86cd604323bb43cbee884bd85d2500
cching01099 <cching01099@gmail.com>
2023-03-14 00:44:58 +0800

    add HomePage's navigation & add logout in Footer

--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -9,7 +9,7 @@ const TodoPage = () => {
   const [inputValue, setInputValue] = useState('');
   const [todos, setTodos] = useState([]);
   const navigate = useNavigate();
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, currentMember } = useAuth();
 
   const handleChange = (value) => {
     setInputValue(value);
@@ -158,7 +158,7 @@ const TodoPage = () => {
   return (
     <div>
       TodoPage
-      <Header />
+      <Header username={currentMember?.name} />
       {/* inputValue這裡是props */}
       <TodoInput
         inputValue={inputValue}
